fe923a8324b50d1437940ee985fb3b00a0c94ed9
Ezequiel <ezemanzur04@gmail.com>
2019-05-17 10:53:53 -0300

    Se inicio el plugin

---
+++ personal.php
@@ -0,0 +1,15 @@
+<?php	
+/*	
+* Plugin Name: Personal-Plugin	
+* Plugin URI: https://github.com/sedici	
+* Description: 	
+* Version: 1.0	
+* Author: SEDICI - Ezequiel Manzur - Maria Marta Vila
+* Author URI: http://sedici.unlp.edu.ar/	
+* Text Domain:   
+* Copyright (c) 2015 SEDICI UNLP, http://sedici.unlp.edu.ar
+* Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.	
+*/
+defined('ABSPATH') or die("Bye bye");
+define('PERSONAL_ROUTE',plugin_dir_path(__FILE__));
+include(PERSONAL_ROUTE.'/includes/functions.php');
