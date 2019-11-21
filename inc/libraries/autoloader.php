1fb387b6b048b9ede88a62868b9d8acdf022900f
Ezequiel <ezemanzur04@gmail.com>
2019-11-21 13:32:58 -0300

    Autoload de clases

---
+++ inc/libraries/autoloader.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * Autoload de clases
+ * @param string $class_name Clase a cargar
+ *
+ */
+
+
+
+spl_autoload_register( function( $class_name ) {
+	// Retorna si no esta incluido en el namespace
+	if ( false === strpos( $class_name, 'Personal' ) ) {
+		return;
+	}
+	$file_parts = explode( '\\', $class_name );
+	$namespace='';
+	for ($i=0; $i < count( $file_parts ) - 1 ; $i++) { 
+		$namespace.= strtolower( $file_parts[ $i ] ).'/';
+	}
+	$namespace.= $file_parts[  count( $file_parts ) -1 ] .'.php';
+
+	 require_once(WP_PLUGIN_DIR.'/'.$namespace);
+}
+);
