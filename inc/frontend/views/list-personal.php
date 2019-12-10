baadc2acb8d5cc9b3ef17a1cdf5838e157cc852e
Ezequiel <ezemanzur04@gmail.com>
2019-12-10 16:30:13 -0300

    Estilos de la lita

--- inc/frontend/views/list-personal.php
+++ inc/frontend/views/list-personal.php
@@ -17,19 +17,33 @@
             $sedici = $this->the_personal_field('sedici');
             $cic = $this->the_personal_field('cic');
             $conicet = $this->the_personal_field('conicet');
-            $categorias = wp_get_post_terms($post->ID, categorias, array("personal"));
+            $categorias = wp_get_post_terms($post->ID, 'categorias', array("personal"));
+            ?>
+            <?php
+            $image = get_the_post_thumbnail_url();
+            $path_image_top = !empty($image) ? $image : plugins_url() . "/personal/assets/images/blank-profile.png";
             ?>
             <div class="card">
-                <?php
-                $image = get_the_post_thumbnail_url();
-                if (!empty($image)): ?><img  class="card-img-top"  src="<?php echo $image; ?>"
-                                             alt="Imagen de <?php echo the_title(); ?>" />
-                <?php else: ?><img
-                    src="<?php echo plugins_url(); ?>/personal/assets/images/blank-profile.png" class="card-img-top"  />
-                <?php endif; ?>
+                <a href="<?php echo get_permalink($post->ID) ?>">
+                    <div class="card-img-top" style="background-image: url('<?php echo $path_image_top ?>'); ">
+                    </div>
+                </a>
+
                 <div class="card-body">
                     <h5 class="card-title">    <?php the_title('<a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a>'); ?></h5>
-                    <div class="card-subtitle mb-2"><?php if (!empty($google_scholar)): ?>
+                    <div class="card-subtitle mb-2"></div>
+                    <p class="card-text small font-weight-bold	"><?php echo $unidad_de_investigacion ?></p>
+                    <!--                    <p class="card-text small font-weight-bold	">-->
+                    <?php //echo $grado_alcanzado
+                    ?><!--</p>-->
+                    <!--<div class="card-button">
+                        <a href="<?php /*get_post_permalink() */
+                    ?>" class="btn btn-primary">Ver más</a>
+                    </div>-->
+                </div>
+                <div class="card-footer">
+                    <div class="footer-redes">
+                        <?php if (!empty($google_scholar)): ?>
                             <a href="<?php echo $google_scholar; ?>" target="_blank"><img
                                         class=" wp-image-16"
                                         src="<?php echo plugins_url() . "/personal/assets/images/google_scholar.png" ?>"
@@ -49,41 +63,12 @@
                                     src="<?php echo plugins_url() . "/personal/assets/images/mailto.gif" ?>"
                                     alt="Mail" width="16" scale="0"></a>
                         <?php endif; ?>
-                        <?php if (!empty($curriculum_vitae)): ?><a href="<?php echo $curriculum_vitae['url']; ?>" target="_blank"><img
+                        <?php if (!empty($curriculum_vitae)): ?><a href="<?php echo $curriculum_vitae['url']; ?>"
+                                                                   target="_blank"><img
                                     src="<?php echo plugins_url() . "/personal/assets/images/cv.png" ?>"
                                     alt="CV" width="16" scale="0"></a>
-                        <?php endif; ?></div>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="<?php get_post_permalink() ?>" class="btn btn-primary">Ver más</a>
-
-                </div>
-                <div class="card-footer">
-                    <?php if (!empty($google_scholar)): ?>
-                        <a href="<?php echo $google_scholar; ?>" target="_blank"><img
-                                    class=" wp-image-16"
-                                    src="<?php echo plugins_url() . "/personal/assets/images/google_scholar.png" ?>"
-                                    alt="google_scholar" width="20" height="20" scale="0"></a>
-                    <?php endif; ?>
-                    <?php if (!empty($reserchgate)): ?><a href="<?php echo $reserchgate; ?>" target="_blank"><img
-                                class=" wp-image-17"
-                                src="<?php echo plugins_url() . "/personal/assets/images/research-gate.png" ?>"
-                                alt="research-gate" width="20" height="20"></a>
-                    <?php endif; ?>
-                    <?php if (!empty($orcid)): ?><a href="<?php echo $orcid; ?>" target="_blank"><img
-                                class=" wp-image-19"
-                                src="<?php echo plugins_url() . "/personal/assets/images/orcid.gif" ?>"
-                                alt="orcid" width="20" height="20" scale="0"> </a>
-                    <?php endif; ?>
-                    <?php if (!empty($email)): ?><a href="mailto:<?php echo $email; ?>" target="_blank"><img
-                                src="<?php echo plugins_url() . "/personal/assets/images/mailto.gif" ?>"
-                                alt="Mail" width="16" scale="0"></a>
-                    <?php endif; ?>
-                    <?php if (!empty($curriculum_vitae)): ?><a href="<?php echo $curriculum_vitae['url']; ?>" target="_blank"><img
-                                src="<?php echo plugins_url() . "/personal/assets/images/cv.png" ?>"
-                                alt="CV" width="16" scale="0"></a>
-                    <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         <?php endwhile; ?>
