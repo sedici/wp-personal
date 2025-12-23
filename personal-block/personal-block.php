c4ab9c8dbdff219c5b1d182ed19e84edc338b76e
Lautaro <ljosinsaller@gmail.com>
2025-12-23 09:29:34 -0300

    Feat : Agrego comentarios al codigo de personal-block

--- personal-block/personal-block.php
+++ personal-block/personal-block.php
@@ -13,7 +13,7 @@
  * @package CreateBlock
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 /**
@@ -21,10 +21,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Behind the scenes, it also registers all assets so they can be enqueued
  * through the block editor in the corresponding context.
  *
+ * This function handles the registration of the block metadata collection and individual block types
+ * defined in the manifest. It supports both the new efficient registration methods introduced in
+ * WordPress 6.7 and 6.8, as well as fallback for older versions (though requires at least 6.7 per headers).
+ *
  * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
  * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
+ *
+ * @return void
  */
-function create_block_personal_block_block_init() {
+function create_block_personal_block_block_init()
+{
 	/**
 	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
 	 * based on the registered block metadata.
@@ -32,8 +39,8 @@ function create_block_personal_block_block_init() {
 	 *
 	 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
 	 */
-	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
-		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+	if (function_exists('wp_register_block_types_from_metadata_collection')) {
+		wp_register_block_types_from_metadata_collection(__DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php');
 		return;
 	}
 
@@ -43,8 +50,8 @@ function create_block_personal_block_block_init() {
 	 *
 	 * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
 	 */
-	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
-		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+	if (function_exists('wp_register_block_metadata_collection')) {
+		wp_register_block_metadata_collection(__DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php');
 	}
 	/**
 	 * Registers the block type(s) in the `blocks-manifest.php` file.
@@ -52,8 +59,8 @@ function create_block_personal_block_block_init() {
 	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
 	 */
 	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
-	foreach ( array_keys( $manifest_data ) as $block_type ) {
-		register_block_type( __DIR__ . "/build/{$block_type}" );
+	foreach (array_keys($manifest_data) as $block_type) {
+		register_block_type(__DIR__ . "/build/{$block_type}");
 	}
 }
-add_action( 'init', 'create_block_personal_block_block_init' );
+add_action('init', 'create_block_personal_block_block_init');
--- personal-block/src/personal-block/render.php
+++ personal-block/src/personal-block/render.php
@@ -10,36 +10,45 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
+// Parse the orderBy attribute into key and direction.
+// Expected format: 'field-direction', e.g., 'date-desc' or 'title-asc'.
 $orderBy = $attributes['orderBy'];
 list($orderby_key, $order_direction) = explode('-', $orderBy);
 
+// Build the WP_Query arguments to fetch 'personal' post type.
 $args = array(
     'post_type' => 'personal',
-    'posts_per_page' => -1, // Show all
+    'posts_per_page' => -1, // Show all posts
     'orderby' => $orderby_key,
     'order' => strtoupper($order_direction),
 );
 
+// If categories are selected, filter the query by those terms.
 if (!empty($attributes['categories'])) {
     $args['tax_query'] = array(
         array(
             'taxonomy' => 'categorias',
-            'field'    => 'term_id',
-            'terms'    => $attributes['categories'],
+            'field' => 'term_id',
+            'terms' => $attributes['categories'],
         ),
     );
 }
 
+// Execute the query.
 $loop = new WP_Query($args);
 
+// Check if there are posts to display.
 if ($loop->have_posts()) {
     echo '<div ' . get_block_wrapper_attributes() . '>';
     // The path to the new view is relative to the plugin root.
     // \Personal\PLUGIN_NAME_DIR is defined in personal.php
+    // Include the view file to render the list of personal items.
     include \Personal\PLUGIN_NAME_DIR . 'inc/frontend/views/list-personal-in-order.php';
     echo '</div>';
 } else {
+    // No posts found, display a fallback message.
     echo '<p ' . get_block_wrapper_attributes() . '>' . __('No hay personal para mostrar', 'personal-block') . '</p>';
 }
 
+// Restore original post data.
 wp_reset_postdata();
